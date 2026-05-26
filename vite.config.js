b1616621f0020d74200bbbc2a78bb9c54b626467
Biglad-graphics <bigladgraphics@gmail.com>
2026-05-26 20:04:36 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/funaab-fantasy-league/'
+  plugins: [react()]
 })
